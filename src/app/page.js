9f9590d17007e40da99adaf864287e9396851716
Diego sales <diegofelipesales23@gmail.com>
2024-01-17 19:57:42 -0400

    criando linkbio para ruyter

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,119 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { FaTwitch, FaInstagram, FaYoutube, FaArrowRight, FaTiktok } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import {
+  TbSquareRoundedLetterG,
+  TbSquareRoundedLetterO,
+  TbSquareRoundedLetterA,
+  TbSquareRoundedLetterT,
+} from "react-icons/tb";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+import profile from "./assets/ruyter-profile.jpg";
+//import back from './assets/back.mp4'
 
 export default function Home() {
   return (
+    <>
+      {/* <video autoPlay loop muted  className={styles.video}>
+        <source src={'./back.mp4'} type="video/mp4" />
+        Seu navegador não suporta o elemento de vídeo.
+      </video> */}
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      
+      <div>
+        <div className={styles.boxImg}>
+          <Image
+            className={styles.logo}
+            src={profile}
+            alt="ruyter profile"
+            width={150}
+            height={150}
+            priority
+          />
+        </div>
+        <div className={styles.boxName}>
+          <h1>Ruyter Poubel</h1>
+          <RiVerifiedBadgeFill size={25} />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      {/* btn para redes sociais */}
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.twitch.tv/ruyterpoubel"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <div>
+            <FaTwitch size={25} />
+            <p className={styles.p}>Siga-me na Twitch</p>
+          </div>
+          <FaArrowRight size={20} />
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.instagram.com/ruyter/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <div>
+            <FaInstagram size={25} />
+            <p className={styles.p}>Siga-me no Instagram</p>
+          </div>
+          <FaArrowRight size={20} />
+        </a>
+        <a
+          href="https://www.tiktok.com/@canaldoruyter"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div>
+            <FaTiktok size={25} />
+            <p className={styles.p}>Siga-me no Tiktok</p>
+          </div>
+          <FaArrowRight size={20} />
+        </a>
+        <a
+          href="https://twitter.com/poubelruyter"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div>
+            <FaXTwitter size={25} />
+            <p className={styles.p}>Siga-me no Twitter</p>
+          </div>
+          <FaArrowRight size={20} />
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.youtube.com/@canaldoruyter"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <div>
+            <FaYoutube size={25} />
+            <p className={styles.p}>Inscreva-se no Youtube</p>
+          </div>
+          <FaArrowRight size={20} />
         </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        
       </div>
+      <footer className={styles.footer}>
+        <p>
+          &copy; Copyright <TbSquareRoundedLetterG size={20} />{" "}
+          <TbSquareRoundedLetterO size={20} />{" "}
+          <TbSquareRoundedLetterA size={20} />{" "}
+          <TbSquareRoundedLetterT size={20} /> 
+          <span>by: @felip.codes</span>
+        </p>
+      </footer>
     </main>
-  )
+    </>
+  );
 }
